10b09fa591b976841b71ae5fd5cae5402d7142b0
Muhammad Aldy Madani Saleh <aldymadani10@gmail.com>
2020-03-18 21:46:52 +0800

    Preventing fragment from recreating by using show and hide fragment
    Signed-off-by: Muhammad Aldy Madani Saleh <aldymadani10@gmail.com>

--- app/src/main/java/com/example/fooddonationapplication/donator/MainDonatorActivity.java
+++ app/src/main/java/com/example/fooddonationapplication/donator/MainDonatorActivity.java
@@ -1,52 +1,113 @@
 package com.example.fooddonationapplication.donator;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
+import com.example.fooddonationapplication.R;
+import com.example.fooddonationapplication.donator.EditProfileInterface.EditProfileFragment;
 import com.example.fooddonationapplication.donator.EventUserInterface.EventFragment;
 import com.example.fooddonationapplication.donator.HistoryUserInterface.DonationHistoryFragment;
-import com.example.fooddonationapplication.donator.EditProfileInterface.EditProfileFragment;
-import com.example.fooddonationapplication.R;
+import com.example.fooddonationapplication.viewmodel.EventViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainDonatorActivity extends AppCompatActivity {
 
     private static final String TAG = "MainDonatorActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    Fragment eventFragment = new EventFragment();
+    Fragment donationHistoryFragment = new DonationHistoryFragment();
+    Fragment editProfileFragment = new EditProfileFragment();
+    Fragment activeFragment;
+    Fragment firstInactiveFragment;
+    Fragment secondInactiveFragment;
+    EventViewModel mViewModel;
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation_donator);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_donator, new EventFragment()).commit();
+        bottomNav = findViewById(R.id.bottom_navigation_donator);
+
+        fragmentManager.beginTransaction().add(R.id.fragment_container_donator, editProfileFragment, "3").commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container_donator, donationHistoryFragment, "2").commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container_donator, eventFragment, "1").commit();
+        mViewModel = new ViewModelProvider(this).get(EventViewModel.class);
+        Log.d(TAG, String.valueOf(mViewModel.getLastSeen()));
+
+        if (mViewModel.getLastSeen() <= 1) {
+            activeFragment = eventFragment;
+            firstInactiveFragment = donationHistoryFragment;
+            secondInactiveFragment = editProfileFragment;
+            Toast.makeText(this, "Last Seen 1", Toast.LENGTH_SHORT).show();
+        } else if (mViewModel.getLastSeen() == 2) {
+            activeFragment = donationHistoryFragment;
+            firstInactiveFragment = eventFragment;
+            secondInactiveFragment = editProfileFragment;
+            Toast.makeText(this, "Last Seen 2", Toast.LENGTH_SHORT).show();
+        } else {
+            activeFragment = editProfileFragment;
+            firstInactiveFragment = donationHistoryFragment;
+            secondInactiveFragment = eventFragment;
+            Toast.makeText(this, "Last Seen 3", Toast.LENGTH_SHORT).show();
         }
+        fragmentManager.beginTransaction().hide(firstInactiveFragment).hide(secondInactiveFragment).show(activeFragment).commit();
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment selectedFragment = null;
                     switch (menuItem.getItemId()) {
                         case R.id.nav_event:
-                            selectedFragment = new EventFragment();
+                            activeFragment = eventFragment;
+                            firstInactiveFragment = donationHistoryFragment;
+                            secondInactiveFragment = editProfileFragment;
+                            mViewModel.setLastSeen(1);
+                            Toast.makeText(MainDonatorActivity.this, "Event", Toast.LENGTH_SHORT).show();
                             break;
                         case R.id.nav_history:
-                            selectedFragment = new DonationHistoryFragment();
+                            activeFragment = donationHistoryFragment;
+                            firstInactiveFragment = eventFragment;
+                            secondInactiveFragment = editProfileFragment;
+                            mViewModel.setLastSeen(2);
+                            Toast.makeText(MainDonatorActivity.this, "History", Toast.LENGTH_SHORT).show();
                             break;
                         case R.id.nav_edit_profile:
-                            selectedFragment = new EditProfileFragment();
+                            activeFragment = editProfileFragment;
+                            firstInactiveFragment = eventFragment;
+                            secondInactiveFragment = donationHistoryFragment;
+                            mViewModel.setLastSeen(3);
+                            Toast.makeText(MainDonatorActivity.this, "Edit", Toast.LENGTH_SHORT).show();
                             break;
                     }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_donator, selectedFragment).commit();
+                    fragmentManager.beginTransaction().hide(firstInactiveFragment).hide(secondInactiveFragment).show(activeFragment).commit();
                     return true;
                 }
             };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fragmentManager.beginTransaction().hide(activeFragment).hide(firstInactiveFragment).hide(secondInactiveFragment).commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fragmentManager.beginTransaction().show(activeFragment).hide(firstInactiveFragment).hide(secondInactiveFragment).commit();
+    }
 }
---
+++ app/src/main/java/com/example/fooddonationapplication/viewmodel/EventViewModel.java
@@ -0,0 +1,24 @@
+package com.example.fooddonationapplication.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class EventViewModel extends ViewModel {
+
+    private int lastSeen, position;
+
+    public int getLastSeen() {
+        return lastSeen;
+    }
+
+    public void setLastSeen(int lastSeen) {
+        this.lastSeen = lastSeen;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
